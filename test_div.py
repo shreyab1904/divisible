9198b33b6eddd3ee3e2fe85355a41945571b6285
shreyab1904 <shreyaborkar0201@gmail.com>
2022-03-28 17:10:59 +0530

    changes added

--- test_div.py
+++ test_div.py
@@ -1,42 +1,40 @@
 import div
+import pytest
 
+@pytest.fixture
+def input():
+    a=25
+    return a
 
-def test_divby5_1():
-    a=10
-    result=div.divby5(a)
+def test_divby5_1(input):
+    result=div.divby5(input)
     assert result==True
 
-def test_divby5_2():
-    a=11
-    result=div.divby5(a)
+def test_divby5_2(input):
+    result=div.divby5(input)
     assert result==False
 
-def test_divby7_1():
-    a=14
-    result=div.divby7(a)
+def test_divby7_1(input):
+    result=div.divby7(input)
     assert result==True
 
-def test_divby7_2():
-    a=10
-    result=div.divby7(a)
+def test_divby7_2(input):
+    result=div.divby7(input)
     assert result==False
 
-def test_divby9_1():
-    a=18
-    result=div.divby9(a)
+def test_divby9_1(input):
+    result=div.divby9(input)
     assert result==True
 
 def test_divby9_2():
     a=10
-    result=div.divby9(a)
+    result=div.divby9(input)
     assert result==False
 
-def test_divby11_1():
-    a=11
-    result=div.divby11(a)
+def test_divby11_1(input):
+    result=div.divby11(input)
     assert result==True
 
-def test_divby11_2():
-    a=10
-    result=div.divby11(a)
+def test_divby11_2(input):
+    result=div.divby11(input)
     assert result==False
